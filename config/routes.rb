f1a63cd6feda5c5540912a68058952443c70b0be
EC2 Default User <105029527+TMoji@users.noreply.github.com>
2022-07-07 15:39:36 +0000

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'meshiterros/new'
-  get 'meshiterros/index'
-  get 'meshiterros/show'
-  get 'meshiterros/edit'
-  get '/about'=>'homes#about'
   root to:'homes#top'
+  devise_for :users
+  resources:post_images,only:[:new,:index,:show]
+  
+  get "/homes/about"=>"homes#about",as:"about"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
